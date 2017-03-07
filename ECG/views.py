217f260237fe775ba6a28679fec8ac167f287d2d
M. Serdar Karaman <m.serdar.karaman@ya.ru>
2017-03-08 00:11:04 +0300

    aa

--- ECG/views.py
+++ ECG/views.py
@@ -195,7 +195,6 @@ def hello():
     global Sdata
     global sr
     url=request.form['myFile']
-	print (url)
     adu=int(request.form['adu'])
     lno=int(request.form['lno'])-1
     sr=int(request.form['sr'])#Sampling rate in Hz ( samples/sec)
